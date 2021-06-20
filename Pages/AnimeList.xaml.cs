c7608a729d0ebfa85d0e028fb8dde1645ceef2f4
LilTaiga <liltaigaishappy@gmail.com>
2021-06-20 17:24:07 -0300

    Anime is now properly grouped by watching status.

--- Pages/AnimeList.xaml.cs
+++ Pages/AnimeList.xaml.cs
@@ -26,14 +26,27 @@ namespace AnimeApp.Pages
     /// </summary>
     public sealed partial class AnimeList : Page
     {
-        public List<Entry> userEntries;                   //A collection of all user entries in his Anilist Account.
+        public List<List> userLists;
+
+        public List<Entry> visibleMedias;
         public ObservableCollection<Entry> medias;        //A collection used to display user entries in the app.
+
+        enum MediaStatus
+        {
+            CURRENT = 1,
+            COMPLETED = 2,
+            PAUSED = 3,
+            DROPPED = 4,
+            PLANNING = 5
+        }
+
+        private MediaStatus currentTab;
 
         public AnimeList()
         {
             this.InitializeComponent();
 
-            userEntries = new List<Entry>();
+            visibleMedias = new List<Entry>();
             medias = new ObservableCollection<Entry>();
 
             GetUserLists();
@@ -45,16 +58,52 @@ namespace AnimeApp.Pages
                 await AnilistAccount.RetrieveLists();
 
             List<List> lists = AnilistAccount.UserLists;
+            userLists = lists;
             
             foreach(List _list in lists)
             {
                 foreach(Entry _entry in _list.entries)
                 {
-                    userEntries.Add(_entry);
+                    visibleMedias.Add(_entry);
                     medias.Add(_entry);
                 }
             }
 
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.SelectedItemContainer != null)
+            {
+                var tabTag = args.SelectedItemContainer.Tag.ToString();
+
+                if (tabTag != currentTab.ToString())
+                {
+                    NavigationView_ChangeTab((MediaStatus)Enum.Parse(typeof(MediaStatus), tabTag));
+                    currentTab = (MediaStatus)Enum.Parse(typeof(MediaStatus), tabTag);
+                }
+            }
+        }
+
+        private void NavigationView_ChangeTab(MediaStatus _tab)
+        {
+            visibleMedias.Clear();
+            medias.Clear();
+
+            foreach(List _list in userLists)
+            {
+                if (_list.status == _tab.ToString())
+                {
+                    visibleMedias.AddRange(_list.entries);
+                    break;
+                }
+            }
+
+            foreach(Entry _entry in visibleMedias)
+            {
+                medias.Add(_entry);
+            }
         }
     }
 }
